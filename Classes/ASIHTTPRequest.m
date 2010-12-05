bd9bf4ca0adf46e1790f2a92a8aa4dc1e53f430c
Anoop Ranganath <anoop@kim.local>
2010-12-06 04:18:24 +0800

    clearDelegatesAndCancel in expiration handler

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -830,7 +830,7 @@ static NSOperationQueue *sharedQueue = nil;
 					{
 						[[UIApplication sharedApplication] endBackgroundTask:backgroundTask];
 						backgroundTask = UIBackgroundTaskInvalid;
-						[self cancel];
+						[self clearDelegatesAndCancel];
 					}
 				});
 			}];
